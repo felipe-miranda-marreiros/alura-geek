4d41176b536c1b99a883c81ce730b8770876078d
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-13 20:53:43 -0300

    Construção de rota usando método Hash

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+import Login from "./src/js/PagesViews/Login.js";
+import Home from "./src/js/PagesViews/Home.js";
+import Products from "./src/js/PagesViews/Products.js";
 
+const getContent = async (fragmentId, callback) => {
+  try {
+    const pages = {
+      login: Login(),
+      home: await Home(),
+      products: await Products(),
+    };
+    callback(pages[fragmentId]);
+  } catch (errors) {
+    console.log(errors);
+  }
+};
+
+const loadContent = () => {
+  const App = document.getElementById("app");
+  let fragmentId = location.hash.substring(1);
+
+  if (!location.hash) {
+    location.hash = "#home";
+  }
+
+  getContent(fragmentId, function (content) {
+    App.innerHTML = "";
+    App.insertAdjacentHTML("afterbegin", content);
+  });
+};
+
+["hashchange", "load"].forEach((event) =>
+  window.addEventListener(event, loadContent)
+);
