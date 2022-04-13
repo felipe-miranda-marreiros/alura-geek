e5fbd81d08b2254a3a372627cc30832a9f8710b2
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-13 20:54:36 -0300

    Construção de componentes reutilizáveis

---
+++ src/js/Components.js
@@ -0,0 +1,16 @@
+const productItemEl = (products) => {
+  return `<div class="products-section__item item--${products.id}">
+      <img
+        src=${products.imageUrl}
+        alt=${products.alt}
+        class="products-section__item-img"
+      />
+      <h3 class="products-section__item-title">${products.name}</h3>
+      <p class="products-section__item-price">R$ ${Number(products.price)
+        .toFixed(2)
+        .replace(".", ",")}</p>
+      <a href=${products.id} class="products-section__item-link">Ver produto</a>
+    </div>`;
+};
+
+export default productItemEl;
