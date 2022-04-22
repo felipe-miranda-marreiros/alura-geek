49875bb8f505394ec9a5a831be354dafe3d9005f
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-04-22 17:32:42 -0300

    [Fix]: responsividade na barra de navegação

--- src/js/components/product.js
+++ src/js/components/product.js
@@ -38,16 +38,11 @@ export const productDescription = (product) => {
 </div>
 <div class="product__content">
   <h2 class="product__title">${product.name}</h2>
-  <h3 class="product__price">${Number(product.price)
+  <h3 class="product__price">R$ ${Number(product.price)
     .toFixed(2)
     .replace(".", ",")}</h3>
   <p class="product__description">
-    Voluptas voluptatum quibusdam similique, class debitis alias
-    maecenas eveniet ridiculus, facilis fusce! Ullam conubia? Sociis,
-    minima malesuada habitasse distinctio sequi aliqua malesuada.
-    Quisque deleniti proin expedita, aliquid litora. Iste recusandae?
-    Commodo, quia ridiculus doloribus vero dictum? Penatibus donec
-    placeat faucibus, dolorum do. Animi porta anim magnam
+    ${product.description}
   </p>
 </div>`;
 };
